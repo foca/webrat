206810e90e7b95a02d37478efb40447df81df999
Simon Rozet <simon@rozet.name>
2008-12-26 06:14:39 +0100

    clean up to test_rack_app

--- spec/webrat/rack/test_rack_app.rb
+++ spec/webrat/rack/test_rack_app.rb
@@ -1,14 +1,13 @@
 class TestRackApp
 
   def call(env)
-    if env["rack.input"]
-      input = env["rack.input"].dup
-      input.rewind
-      env["rack.input"] = input.read
-    end
-
     [200, {"Content-Type" => "text/plain"}, [
-      env.to_yaml
+      env.tap do |env|
+        if input = env["rack.input"]
+          input.rewind
+          env["rack.input"] = input.read
+        end
+      end.to_yaml
     ]]
   end
 end
