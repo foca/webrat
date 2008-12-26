9a1f8f4c9b8ff7f1d16fcdc60b7092593a8c54fe
Simon Rozet <simon@rozet.name>
2008-12-26 06:41:39 +0100

    woot it works

--- lib/webrat/sinatra.rb
+++ lib/webrat/sinatra.rb
@@ -1,18 +1,10 @@
 require 'webrat/rack'
 require 'sinatra'
-require 'sinatra/test/methods'
 
 module Webrat
   class SinatraSession < RackSession #:nodoc:
-    include Sinatra::Test::Methods
-
-    %w(get head post put delete).each do |verb|
-      define_method(verb) do |*args| # (path, data, headers = nil)
-        path, data, headers = *args
-        params = data.merge({:env => headers || {}})
-        self.__send__("#{verb}_it", path, params)
-        follow! while @response.redirect?
-      end
+    def initialize(app=Sinatra.build_application)
+      @app = app
     end
     
   end
