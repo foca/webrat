30b4c5a1a94c6cc6c6feefb62bf73a6b028cbc7c
Simon Rozet <simon@rozet.name>
2008-12-28 22:45:14 +0100

    more of the last

--- spec/webrat/sinatra/test_sinatra_app.rb
+++ spec/webrat/sinatra/test_sinatra_app.rb
@@ -1,19 +1,47 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
-require "webrat/sinatra"
+require "sinatra"
 
 class Sinatra::Application
-  # Override this to prevent Sinatra from barfing on the options passed from RSpec
   def self.load_default_options_from_command_line!
   end
 end
 
-Sinatra::Application.default_options.merge!(
-  :env          => :test,
-  :run          => false,
-  :raise_errors => true,
-  :logging      => false
-)
+disable :reload
+
+use_in_file_templates!
 
 get "/" do
-  "Hello World"
+  erb :home
 end
+
+get "/go" do
+  erb :go
+end
+
+post "/go" do
+  @user = params[:email]
+  erb :hello
+end
+
+use_in_file_templates!
+__END__
+@@ layout
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <title>sinatra testing with webrat</title>
+  <body>
+    <%= yield %>
+  </body>
+</html>
+
+@@ home
+<p> visit <a href="/go">there</a></p>
+
+@@ go
+<form method="post" action="/go">
+  <input type="text" name="email" id="email">
+  <input type="submit" />
+</form>
+
+@@ hello
+<p>hello, <%= @user %></p>
+
