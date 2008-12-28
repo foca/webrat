8fc328392a12fe4402797f6143c53416f3e7a3a6
Simon Rozet <simon@rozet.name>
2008-12-28 22:44:41 +0100

    trying to fix sinatra

--- spec/webrat/sinatra/session_spec.rb
+++ spec/webrat/sinatra/session_spec.rb
@@ -1,13 +1,21 @@
-require File.expand_path(File.dirname(__FILE__) + '/helper')
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + "/../../../lib")
+require "webrat/sinatra"
+require File.expand_path(File.dirname(__FILE__) + "/test_sinatra_app")
 
 describe Webrat::SinatraSession do
   before(:each) do
-    Webrat.configuration.mode == :sinatra
+    Webrat.configuration.mode = :sinatra
     @session = Webrat::SinatraSession.new
   end
 
+  it "should set mode to :sinatra" do
+    Webrat.configuration.mode.should == :sinatra
+  end
+
   it "should work" do
-    @session.get "/"
-    @session.response_body.should == "Hello World"
+    @session.request_page("/", "get", "")
+    @session.click_link "there"
+    # TODO: use webrat method instead
+    @session.response_body.should =~ /form/
   end
 end
