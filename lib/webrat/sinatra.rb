215aef03fc7023236593b8d7e95c29776c733874
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2008-12-28 03:42:23 -0200

    Don't run the sinatra app

--- lib/webrat/sinatra.rb
+++ lib/webrat/sinatra.rb
@@ -1,11 +1,12 @@
 require 'webrat/rack'
 require 'sinatra'
 
+disable :run
+
 module Webrat
   class SinatraSession < RackSession #:nodoc:
     def initialize(app=Sinatra.build_application)
       @app = app
     end
-    
   end
 end
